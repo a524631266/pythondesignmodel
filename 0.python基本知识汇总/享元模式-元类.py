e600b13a4d33602f372538e0d2dc5f03f910a0dd
zhangll <524631266@qq.com>
2018-10-15 00:25:30 +0800

    添加元类方法

--- 0.python基本知识汇总/享元模式-元类.py
+++ 0.python基本知识汇总/享元模式-元类.py
@@ -1,13 +1,28 @@
+class addclass:
+    # names = ["aaa"]
+    def __init__(self,name):
+        self.name = name
+    def print(self):
+        print(self.name)
 
 def add(a,b):
     return a + b
-# 使用type添加一个add2类
+# 使用type添加一个add2类 object.__new__(cls)
 add2 = type("add2",(),{"name":"zhanglll"})
+# 元类用来干什么? 元类是用来拦截并扩展 "类的创建" 类似装饰器功能
+
 
 def main():
     # print(add.__class__.__class__) # <class 'type'>
     print(add2().name)
     print(add2.name)
-    print("111")
+    print(add2.__dict__)
+    print(addclass.__dict__) # __dict__可以显示类的基本属性
+    print("#############")
+    a = addclass("name")
+    print(addclass.print)
+    a.print() 
+    addclass.__dict__["names"].append(222)
+    a.print()
 if __name__ == '__main__':
     main()
